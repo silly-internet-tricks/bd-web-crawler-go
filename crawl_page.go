cb47641d3805043eb5f183a2aae27e56fc5bfa10
Josh Parker <joshBBparker@gmail.com>
2024-09-02 13:04:40 -0600

    try crawlin' concurrently 😎

--- crawl_page.go
+++ crawl_page.go
@@ -6,19 +6,32 @@ import (
 	"strings"
 )
 
-func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) (p map[string]int, err error) {
-	baseUrl, err := url.Parse(rawBaseURL)
-	if err != nil {
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	defer cfg.mu.Unlock()
+	cfg.mu.Lock()
+	_, ok := cfg.pages[normalizedURL]
+	if ok {
+		cfg.pages[normalizedURL]++
 		return
 	}
 
+	cfg.pages[normalizedURL] = 1
+	isFirst = true
+	return
+}
+
+func (cfg *config) crawlPage(rawCurrentURL string) {
+	defer func() {
+		cfg.wg.Done()
+		<-cfg.concurrencyControl
+	}()
+	cfg.concurrencyControl <- struct{}{}
 	currentUrl, err := url.Parse(rawCurrentURL)
 	if err != nil {
 		return
 	}
 
-	if baseUrl.Host != currentUrl.Host {
-		p = pages
+	if cfg.baseURL.Host != currentUrl.Host {
 		return
 	}
 
@@ -27,15 +40,11 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) (p map[st
 		return
 	}
 
-	_, ok := pages[normalizedURL]
-	if ok {
-		pages[normalizedURL]++
-		p = pages
+	isFirst := cfg.addPageVisit(normalizedURL)
+	if !isFirst {
 		return
 	}
 
-	pages[normalizedURL] = 1
-	p = pages
 	fmt.Printf("now crawling new page: %v\n", normalizedURL)
 	html, err := getHTML("https://" + normalizedURL)
 	if err != nil {
@@ -46,17 +55,16 @@ func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) (p map[st
 		return
 	}
 
-	urls, err := getURLsFromHTML(html, rawBaseURL)
+	urls, err := getURLsFromHTML(html, cfg.baseURL.String())
 	if err != nil {
 		return
 	}
 
 	for _, url := range urls {
-		p, err = crawlPage(rawBaseURL, url, p)
+		cfg.wg.Add(1)
+		go cfg.crawlPage(url)
 		if err != nil {
 			return
 		}
 	}
-
-	return
 }
--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"sync"
 )
+
+const maxConcurrency = 10
 
 func main() {
 	if len(os.Args) < 2 {
@@ -19,13 +23,31 @@ func main() {
 
 	fmt.Printf("starting crawl of: %v\n", os.Args[1])
 	pages := make(map[string]int)
-	p, err := crawlPage(os.Args[1], os.Args[1], pages)
+	baseURL, err := url.Parse(os.Args[1])
+	if err != nil {
+		log.Fatalf("could not url parse the argument that was passed in!")
+	}
+
+	mu := &sync.Mutex{}
+	concurrencyControl := make(chan struct{}, maxConcurrency)
+	wg := &sync.WaitGroup{}
+	cfg := config{
+		pages,
+		baseURL,
+		mu,
+		concurrencyControl,
+		wg,
+	}
+
+	cfg.wg.Add(1)
+	go cfg.crawlPage(os.Args[1])
+	cfg.wg.Wait()
 	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
 	}
 
-	for key, value := range p {
+	for key, value := range cfg.pages {
 		fmt.Printf("I found *%v* links to the page: %v\n", value, key)
 	}
 }
